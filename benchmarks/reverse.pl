e2faa81f54fbe64e81c3520c828389fe33e2bb77
Michael Hanus <mh@informatik.uni-kiel.de>
2011-02-13 14:53:56 +0100

    Benchmarks added

---
+++ benchmarks/reverse.pl
@@ -0,0 +1,32 @@
+% Prolog benchmark: naive reverse of a user-defined list
+
+add(o,N,N).
+add(s(X),Y,s(Z)) :- add(X,Y,Z).
+
+double(X,R) :- add(X,X,R).
+
+mult(o,_,o).
+mult(s(X),Y,Z) :- mult(X,Y,XY), add(Y,XY,Z).
+
+two(s(s(o))).
+four(F) :- two(T), double(T,F).
+nat16(N) :- four(F), mult(F,F,N).
+nat256(N) :- nat16(M), mult(M,M,N).
+nat4096(R) :- nat256(M), nat16(N), mult(M,N,R).
+
+app(nil,Xs,Xs).
+app(cons(X,Xs),Ys,cons(X,Zs)) :- app(Xs,Ys,Zs).
+
+reverse(nil,nil).
+reverse(cons(X,Xs),R) :- reverse(Xs,Zs), app(Zs,cons(X,nil),R).
+
+natList(o,nil).
+natList(s(X),cons(s(X),Z)) :- natList(X,Z).
+
+isList(nil,true).
+isList(cons(_,Xs),R) :- isList(Xs,R).
+
+goal3(R) :- nat4096(R1), natList(R1,R2), reverse(R2,R3), isList(R3,R).
+
+main :- goal3(R), write(R), nl, halt.
+
