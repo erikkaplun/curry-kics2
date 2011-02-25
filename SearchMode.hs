c738032a9c0aea562d8c9d22ab0eb34750d31943
Bjoern Peemoeller <bjp@palmer.informatik.uni-kiel.de>
2011-02-25 16:50:02 +0100

    Module SearchMode removed as its content is covered by the CompilerOpts

--- SearchMode.hs
+++
@@ -1,9 +0,0 @@
-module SearchMode (SearchMode (..)) where
-
-data SearchMode
-  = NoSearch -- no search
-  | DFS      -- depth first search
-  | BFS      -- bredth first search
-  | IterDFS  -- iterative depth first search
-  | PAR      -- parallel search
-    deriving (Eq,Read,Show)
