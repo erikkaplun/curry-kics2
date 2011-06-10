f250fca56905043b0682885ea24a3e0fb6a47226
Björn Peemöller <bjp@informatik.uni-kiel.de>
2011-06-10 11:34:08 +0200

    Added logging for bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
+LOGFILE=bootstrap.log
 
 # Bootstrap KiCS2 compiler
 
 # make everything clean:
 make clean
 
+# start logging
+echo "Bootstrapping started at `date`" > $LOGFILE
+
 # Create kics2 via PAKCS (stage 1)
-make Compile.state
+echo "Compiling stage 1" >> $LOGFILE
+make Compile.state 2>&1 | tee -a $LOGFILE
 
 # Create kics2 via kics2 (stage 2)
-make Compile
+echo "Compiling stage 2" >> $LOGFILE
+make Compile 2>&1 | tee -a $LOGFILE
 
 # Create REPL via KiCS2, stage 2
-make REPL
+echo "Compiling REPL" >> $LOGFILE
+make REPL 2>&1 | tee -a $LOGFILE
+
+echo "Bootstrapping finished at  `date`" >> $LOGFILE
 
 # Now, kics2 should work with the bootstrapped compiler, i.e., you can
 # install the complete system:
