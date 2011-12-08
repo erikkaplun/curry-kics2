57b3ef054062b9451686121c1436cea478161a70
Michael Hanus <mh@informatik.uni-kiel.de>
2011-12-08 14:19:45 +0100

    Path to ghc binary added to Installation and used in REPL

--- src/CompileBoot.hs
+++ src/CompileBoot.hs
@@ -1,8 +1,7 @@
--- /home/mh/home/curry/idcompiler/idc -v 3  -i /home/mh/home/curry/idcompiler/lib -i /home/mh/home/curry/idcompiler/lib/meta Compile.curry
+-- Main module for KiCS2 compiler
 
 module Main where
 import Basics
 import Curry_Compile
 main = evalDIO d_C_main
 
--- ghc -O2 --make -v1 -XMultiParamTypeClasses -XFlexibleInstances -XRelaxedPolyRec  -i/home/mh/home/curry/idcompiler/runtime:/home/mh/home/curry/idcompiler/runtime/idsupplyinteger:./.curry/kics2/:/home/mh/home/curry/idcompiler/lib/.curry/kics2/:/home/mh/home/curry/idcompiler/lib/meta/.curry/kics2/ Compile.hs
