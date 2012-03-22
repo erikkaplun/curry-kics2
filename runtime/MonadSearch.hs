f0d69e3e6d00c1658cea9858898fe75bcdfe6964
Björn Peemöller <bjp@informatik.uni-kiel.de>
2012-03-22 11:54:55 +0100

    Removed some warnings

--- runtime/MonadSearch.hs
+++ runtime/MonadSearch.hs
@@ -7,23 +7,22 @@ import Control.Monad.State.Strict as Strict
 
 class MonadPlus m => MonadSearch m where
   szero :: Int -> FailInfo -> m a
-  szero _ _ = mzero
   splus :: ID -> m a -> m a -> m a
-  splus _ = mplus
   ssum  :: ID -> [m a] -> m a
-  ssum _ = msum
   constrainMSearch :: Constraints -> m a -> m a
 
-
+  szero _ _ = mzero
+  splus _   = mplus
+  ssum  _   = msum
 
 instance MonadSearch m => MonadSearch (Lazy.StateT a m) where
   splus i mx my = Lazy.StateT (\s -> splus i (Lazy.runStateT mx s) (Lazy.runStateT my s))
   ssum  i mxs   = Lazy.StateT (\s -> ssum i (map (flip Lazy.runStateT s) mxs))
-  szero cd info = Lazy.StateT (\s -> szero cd info)
+  szero cd info = Lazy.StateT (\_ -> szero cd info)
   constrainMSearch cs mx = Lazy.StateT (\s -> constrainMSearch cs (Lazy.runStateT mx s))
 
 instance MonadSearch m => MonadSearch (Strict.StateT a m) where
   splus i mx my = Strict.StateT (\s -> splus i (Strict.runStateT mx s) (Strict.runStateT my s))
   ssum  i mxs   = Strict.StateT (\s -> ssum i (map (flip Strict.runStateT s) mxs))
-  szero cd info = Strict.StateT (\s -> szero cd info)
+  szero cd info = Strict.StateT (\_ -> szero cd info)
   constrainMSearch cs mx = Strict.StateT (\s -> constrainMSearch cs (Strict.runStateT mx s))
