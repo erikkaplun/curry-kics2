d632767c2526ff13f4c4f186414271da0e9a3e56
Fabian Reck <fre@informatik.uni-kiel.de>
2012-06-29 14:46:39 +0200

    moved libs to a separate repository and imported them as submodule

--- lib/External_SetFunctions.hs
+++
@@ -1,4 +0,0 @@
-
-
-external_d_C_cover :: Coverable a => a -> ConstStore -> a
-external_d_C_cover x _ = cover x
--- lib/External_Unsafe.hs
+++
@@ -1,7 +0,0 @@
-import System.IO.Unsafe(unsafePerformIO)
-import qualified Curry_Prelude as CP
-
-external_d_C_unsafePerformIO :: CP.C_IO a -> ConstStore -> a
-external_d_C_unsafePerformIO io cs = unsafePerformIO  (toIO io cs)
-
------------------------------------------------------------------------
