2be0e6ca4cf266cb5d51f010db3ae63571d3b93c
Björn Peemöller <bjp@informatik.uni-kiel.de>
2012-07-30 14:50:29 +0200

    Kics2 batch script added

--- scripts/cymake.bat
+++ scripts/cymake.bat
@@ -1,3 +1,4 @@
+@echo off
 REM Batch file to call the front end for Curry
 
 REM The installation directory of KiCS2
@@ -6,4 +7,4 @@ set KICS2HOME=`echo KICS2HOME must be defined here!`
 REM Set 'cymake' executable
 set CYMAKE = %KICS2HOME%/bin/.local/cymake.exe
 
-%CYMAKE% --extended $*
+%CYMAKE% --extended %*
