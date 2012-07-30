d80cde8287fefe1156b76ddf013543e97081447d
Björn Peemöller <bjp@informatik.uni-kiel.de>
2012-07-30 14:36:15 +0200

    Created cymake batch file

---
+++ scripts/cymake.bat
@@ -0,0 +1,9 @@
+REM Batch file to call the front end for Curry
+
+REM The installation directory of KiCS2
+set KICS2HOME=`echo KICS2HOME must be defined here!`
+
+REM Set 'cymake' executable
+set CYMAKE = %KICS2HOME%/bin/.local/cymake.exe
+
+%CYMAKE% --extended $*
