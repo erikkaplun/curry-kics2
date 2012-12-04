d5eab0d467bb28e66be4a115f0acf0c185d2fcdd
Björn Peemöller <bjp@informatik.uni-kiel.de>
2012-12-04 14:33:48 +0100

    Simplified FailInfo

--- runtime/FailInfo.hs
+++ runtime/FailInfo.hs
@@ -6,23 +6,13 @@ module FailInfo where
 
 import Data.List
 
--- data FailCause
---   | MatchFailure       String
---   | UnificationFailure String String
---   | CallToFailed
-
 type Call  = (String, [String])
 type Cause = String
 
 data FailInfo = FailInfo
   { failTrace :: [Call]
   , failCause :: Cause
-  } deriving (Eq)
-
-instance Show FailInfo where
-  show (FailInfo trace cause) =
-      "A failure occured for the following reason:\n" ++ cause
-   ++ "\n\nTrace:\n" ++ unlines (map (uncurry fmtCall) trace)
+  } deriving (Eq, Show)
 
 defFailInfo :: FailInfo
 defFailInfo = FailInfo [] "Unspecified failure"
@@ -37,11 +27,8 @@ consFail fun arg = FailInfo [] msg
 
 unificationFail :: String -> String -> FailInfo
 unificationFail arg1 arg2 = FailInfo [] msg
-  where msg = "Unification failed for terms " ++ arg1 ++ " and " ++ arg2
+  where msg = "Unification failed for terms `" ++ arg1 ++ "' and `" ++ arg2 ++ "'"
 
 -- trace a failure in an argument position
 traceFail :: String -> [String] -> FailInfo -> FailInfo
 traceFail fun args fi = fi { failTrace = (fun, args) : failTrace fi }
-
-fmtCall ::  String -> [String] -> String
-fmtCall fun args = intercalate " " (fun : args)
